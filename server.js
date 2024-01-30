cf8f42b646fb13bfd1f603ee405b113064e799ee
Viktoriia Kostak <viktoriakostak570@gmail.com>
2024-01-30 17:24:54 +0300

    blog

---
+++ server.js
@@ -0,0 +1,47 @@
+const express = require("express");
+const morgan = require("morgan");
+const mongoose = require("mongoose");
+require("dotenv").config();
+const methodOverride = require("method-override");
+const postRoutes = require("./routes/post-routes");
+const postApiRoutes = require("./routes/api-post-routes");
+const contactRoutes = require("./routes/contact-routes");
+const createPath = require("./helpers/create-path");
+
+const app = express();
+
+app.set("view engine", "ejs");
+
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then((res) => console.log("Connect to db"))
+  .catch((error) => console.log(error));
+
+app.listen(process.env.PORT, (error) => {
+  error
+    ? console.log(error)
+    : console.log(`Listening port ${process.env.PORT}`);
+});
+
+app.use(
+  morgan(":method :url :status :res[content-length] - :response-time ms")
+);
+app.use(express.static("styles"));
+
+app.use(methodOverride("_method"));
+
+app.use(express.urlencoded({ extended: false }));
+
+app.get("/", (req, res) => {
+  const title = "Home";
+  res.render(createPath("index"), { title });
+});
+
+app.use(postRoutes);
+app.use(contactRoutes);
+app.use(postApiRoutes);
+
+app.use((req, res) => {
+  const title = "Error Page";
+  res.status(404).render(createPath("error"), { title });
+});
